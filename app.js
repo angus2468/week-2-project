b53178f632bb36012cb05bf1584fbd2196686d0d
Angus <angus.baker2468@gmail.com>
2025-01-19 19:40:31 +0000

    Did some stretch goals

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ const thumbnailContainer = document.getElementById("thumbnailContainer");
 const backgroundImage = document.getElementById("backgroundImage");
 const leftButton = document.getElementById("leftButton");
 const rightButton = document.getElementById("rightButton");
+const arrowKeys = document.getElementById("arrowKeys");
 let currentImage = 0;
 
 let images = [
   {
-    src: "https://images.unsplash.com/photo-1521753643072-122f97ed86e4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/assets/ForestOne2000w.png",
     alt: "An image of a forst with a lot of bluebells",
+    srcset:
+      "/assets/ForestOne2000w.png 2000w, /assets/ForestOne1000w.png 1000w, /assets/ForestOne800w.png 800w, /assets/ForestOne400w.png 400w",
   },
   {
-    src: "https://images.unsplash.com/photo-1542202229-7d93c33f5d07?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/assets/ForestTwo2000w.png",
     alt: "An image of a forst",
+    srcset:
+      "/assets/ForestTwo2000w.png 2000w, /assets/ForestTwo2000w.png 1000w, /assets/ForestTwo800w.png 800w, /assets/ForestTwo400w.png 400w",
   },
   {
-    src: "https://images.unsplash.com/photo-1523228046673-5dbc4d067caf?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/assets/ForestThree2000w.png",
     alt: "An image of a stream going through a forest",
+    srcset:
+      "/assets/ForestThree2000w.png 2000w, /assets/ForestThree1000w.png 1000w, /assets/ForestThree800w.png 800w, /assets/ForestThree400w.png 400w",
   },
   {
-    src: "https://images.unsplash.com/photo-1472017454701-20a80cfeaac5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/assets/ForestFour2000w.png",
     alt: "An image of a forst that goes to the waterline of a lake",
+    srcset:
+      "/assets/ForestFour2000w.png 2000w, /assets/ForestFour1000w.png 1000w, /assets/ForestFour800w.png 800w, /assets/ForestFour400w.png 400w",
+  },
+  {
+    src: "/assets/ForestFive2000w.png",
+    alt: "An image of rock in a forest in the fog",
+    srcset:
+      "/assets/ForestFive2000w.png 2000w, /assets/ForestFive1000w.png 1000w, /assets/ForestFive800w.png 800w, /assets/ForestFive400w.png 400w",
   },
 ];
 
@@ -27,7 +42,7 @@ function createThumbnail() {
   images.forEach(function (image, index) {
     let imageElement = document.createElement("img");
     imageElement.src = image.src;
-    // imageElement.scrset = image.scrset
+    imageElement.scrset = image.scrset;
     imageElement.alt = image.alt;
     imageElement.addEventListener("click", function () {
       createBackgroundImage(image);
@@ -52,7 +67,7 @@ function createBackgroundImage(img) {
   backgroundImage.innerHTML = "";
   const backImage = document.createElement("img");
   backImage.src = img.src;
-  // backImage.srcset = img.srcset
+  backImage.srcset = img.srcset;
   backImage.alt = img.alt;
   backImage.classList.add("backgroundImage");
   backgroundImage.appendChild(backImage);
@@ -68,11 +83,20 @@ leftButton.addEventListener("click", function () {
     createBackgroundImage(images[currentImage]);
   }
 });
-leftButton.addEventListener("keypress", function (leftArrow) {
-  if ((leftArrow.key = "keyleft")) {
+arrowKeys.addEventListener("keydown", function (leftArrow) {
+  if (leftArrow.key == "ArrowLeft") {
     currentImage -= 1;
     if (currentImage < 0) {
       currentImage = images.length - 1;
+      createBackgroundImage(images[currentImage]);
+    } else {
+      createBackgroundImage(images[currentImage]);
+    }
+  }
+  if (leftArrow.key == "ArrowRight") {
+    currentImage += 1;
+    if (currentImage >= images.length) {
+      currentImage = 0;
       createBackgroundImage(images[currentImage]);
     } else {
       createBackgroundImage(images[currentImage]);
