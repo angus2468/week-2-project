92a4a3a81d3719cd45e6c0e934751b4c411b7634
angus2468 <angus.baker2468@gmail.com>
2025-01-20 00:10:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,34 +7,34 @@ let currentImage = 0;
 
 let images = [
   {
-    src: "./assets/ForestOne2000w.png",
+    src: "assets/ForestOne2000w.png",
     alt: "An image of a forst with a lot of bluebells",
     srcset:
-      "./assets/ForestOne2000w.png 2000w, ./assets/ForestOne1000w.png 1000w, ./assets/ForestOne800w.png 800w, ./assets/ForestOne400w.png 400w",
+      "assets/ForestOne2000w.png 2000w, assets/ForestOne1000w.png 1000w, assets/ForestOne800w.png 800w, assets/ForestOne400w.png 400w",
   },
   {
-    src: "./assets/ForestTwo2000w.png",
+    src: "assets/ForestTwo2000w.png",
     alt: "An image of a forst",
     srcset:
-      "./assetsForestTwo2000w.png 2000w, ./assets/ForestTwo2000w.png 1000w, ./assets/ForestTwo800w.png 800w, ./assets/ForestTwo400w.png 400w",
+      "assetsForestTwo2000w.png 2000w, assets/ForestTwo2000w.png 1000w, assets/ForestTwo800w.png 800w, assets/ForestTwo400w.png 400w",
   },
   {
     src: "assets/ForestThree2000w.png",
     alt: "An image of a stream going through a forest",
     srcset:
-      "./assets/ForestThree2000w.png 2000w, ./assets/ForestThree1000w.png 1000w, ./assets/ForestThree800w.png 800w, ./assets/ForestThree400w.png 400w",
+      "assets/ForestThree2000w.png 2000w, assets/ForestThree1000w.png 1000w, assets/ForestThree800w.png 800w, assets/ForestThree400w.png 400w",
   },
   {
-    src: "./assets/ForestFour2000w.png",
+    src: "assets/ForestFour2000w.png",
     alt: "An image of a forst that goes to the waterline of a lake",
     srcset:
-      "./assets/ForestFour2000w.png 2000w, ./assets/ForestFour1000w.png 1000w, ./assets/ForestFour800w.png 800w, ./assets/ForestFour400w.png 400w",
+      "assets/ForestFour2000w.png 2000w, assets/ForestFour1000w.png 1000w, assets/ForestFour800w.png 800w, assets/ForestFour400w.png 400w",
   },
   {
-    src: "./assets/ForestFive2000w.png",
+    src: "assets/ForestFive2000w.png",
     alt: "An image of rock in a forest in the fog",
     srcset:
-      "./assets/ForestFive2000w.png 2000w, ./assets/ForestFive1000w.png 1000w, ./assets/ForestFive800w.png 800w, ./assets/ForestFive400w.png 400w",
+      "assets/ForestFive2000w.png 2000w, assets/ForestFive1000w.png 1000w, assets/ForestFive800w.png 800w, assets/ForestFive400w.png 400w",
   },
 ];
 
